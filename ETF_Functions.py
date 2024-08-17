206a9d292a43bb95a782678132d561bf92094b99
splanky0314 <splanky0314@gmail.com>
2024-08-18 01:44:42 +0900

    FEAT: endpoint Backtest_result/ development completed

--- ETF_Functions.py
+++ ETF_Functions.py
@@ -5,7 +5,7 @@ import json
 def get_ticker_by_company_name(company_name):
     search = yq.search(company_name)
     quotes = search.get('quotes', [])
-    
+
     if not quotes:
         result = json.dumps({'success':False, 'message':f"Wrong Company Name: {company_name} (Excluded from calculation)"})
         print(result)
@@ -17,3 +17,14 @@ def get_ticker_by_company_name(company_name):
     result = json.dumps({'success': True, 'ticker':ticker})
     print(result)
     return result
+
+# Function to get ETF price data 
+def get_etf_price_data(tickers: list, startyear: str):
+    # tickers = ['AAPL', 'MSFT', 'NVDA', 'GOOG', 'XLK']
+    etf = yf.Tickers(tickers)
+    data = etf.history(start=startyear, actions=False)
+    data.drop(['Open', 'High', 'Low', 'Volume'], inplace=True, axis=1)
+    data = data.droplevel(0, axis=1)
+    data.ffill(inplace=True)
+    df = data.resample('W').last()
+    return df
--- main.py
+++ main.py
@@ -1,6 +1,53 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+import pandas as pd
+import quantstats as qs
 
-from ETF_Functions import get_ticker_by_company_name
+from io import BytesIO
+import base64
+import matplotlib
+from PIL import Image
+import tempfile
+
+from ETF_Functions import get_ticker_by_company_name, get_etf_price_data
+from backtesting_engine import GEMTU772
+
+matplotlib.use('Agg') # Engine reset issue solution code (TkAgg->Agg)
+
+load_dotenv()
+
+genai.configure(api_key=os.environ.get('GEMINI_API_KEY'))
+
+# Create the model
+# See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    }
+]
 
 # fastapi
 app = FastAPI()
@@ -13,3 +60,43 @@ def test():
 def get_ticker(company_name: str):
     result = get_ticker_by_company_name(company_name)
     return result
+
+@app.post('/Backtest_result')
+async def generate_report(request: Request):
+    print("BACKTEST")
+    print(request.json())
+    print(request.body())
+    data = await request.json()
+    print(data)
+
+    if data is None:
+        return {"error": "No input data provided"}, 400
+
+    cs_model = data.get('cs_model')
+    ts_model = data.get('ts_model')
+    tickers = data.get('tickers')
+    startyear = data.get('startyear')
+
+    if not cs_model or not ts_model:
+        return {"error": "Missing model selection"}, 400
+
+    df = get_etf_price_data(tickers, startyear)
+    
+    engine = GEMTU772(df)
+    res = engine.run(cs_model=cs_model, ts_model=ts_model, cost=0.0005)
+    port_weights, port_asset_rets, port_rets = res
+
+    port_weights_img, asset_performance_img, portfolio_performance_img = engine.performance_analytics(port_weights, port_asset_rets, port_rets)
+
+    print("============")
+    print(res)
+    print("=====")
+    # print(type(res))
+    # print(port_weights_img)
+    # print(type(port_weights_img))
+
+    return {
+        "port_weights_img": port_weights_img,
+        "asset_performance_img": asset_performance_img,
+        "portfolio_performance_img": portfolio_performance_img
+    }
